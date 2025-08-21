422a3b65a97ade6738ef29f93477a6f03442bdd4
ajsafedrive <156267981+ajsafedrive@users.noreply.github.com>
2025-08-21 11:48:25 -0700

    Add files via upload

---
+++ LargestOfThree.java
@@ -0,0 +1,78 @@
+ // LargestOfThree.java
+ // aJ 8/21/25
+ //References: Gemini, ChatGPT, Deepseek
+ // This program will input three ints from the user
+ // Processing will be finding the largest of three numbers with nested if statements.
+ // Output will be an understandable statement of the largest number.
+ // Example: You input the value of 55 for num2 and that was the largest of the three.
+
+ // I need to import the scanner class from the Java.Util Package/Library
+ import java.util.Scanner;
+
+ public class LargestOfThree {
+    public static void main(String[] args) {
+
+        System.out.println("\n\nWelcome to My Largest of Three Program");
+
+        // variables needed for this program.
+        // initialize num1 to 0
+        // 0 is assigned to num1
+        int num1 = 0;
+        int num2 = 0;
+        int num3 = 0;
+        int largest = 0;
+
+        // Get input from the users
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the users for the first int
+        System.out.println("\n Please enter a whole number for num1: ");
+        num1 = scanner.nextInt();
+        // output the user's input to make sure my program is working
+        // read about concatenation
+        System.out.println("\n the user entered " + num1 + " for num1");
+
+        // Prompt the users for the second int
+        System.out.println("\n Please enter a whole number for num2: ");
+        num2 = scanner.nextInt();
+        // output the user's input to make sure my program is working
+        System.out.println("\n the user entered " + num2 + " for num2");
+
+        // Prompt the users for the third int
+        System.out.println("\n Please enter a whole number for num3: ");
+        num3 = scanner.nextInt();
+        // output the user's input to make sure my program is working
+        System.out.println("\n the user entered " + num3 + " for num3");
+
+        // Processing
+        // Use nested if statements to find the largest.
+        if (num1 > num2) {
+            if (num1 > num3) {
+                // This is the true part
+                largest = num1;
+            }
+            else {
+                // this is the false part
+                largest = num3;
+            }
+        }
+        else { if (num2 > num3) {
+            // This is the true part
+            largest = num2;
+        }
+        else {
+            // this is the false part
+            largest = num3;
+        }
+
+        }
+
+        // output meaningful information to the user
+        System.out.println(" \n You entered " + num1 + " for the value of num1.\n");
+        System.out.println(" \n You entered " + num2 + " for the value of num2.\n");
+        System.out.println(" \n You entered " + num3 + " for the value of num3.\n");
+        System.out.println("\n The largest number is " + largest);
+
+
+    }
+}
